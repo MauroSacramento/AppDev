91f1b0859aa192093fec18dc8e03946191f1a0f8
Maurosacramento <maurosacramento35@gmail.com>
2025-03-20 16:46:25 +0000

    Criar a rota de cadastro de dev

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const routes = require('./router')
 const mongoose = require('mongoose');
 
 //9Dfm33gHpdSa3eJ
-//mongoose.connect("mongodb+srv://omnistack:9Dfm33gHpdSa3eJ@cluster0.pny26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://omnistack:9Dfm33gHpdSa3eJ@cluster0.pny26.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 app.use(express.json());
 app.use("/api", routes)
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,16 +1,30 @@
 const { Router } = require('express');
 const axios = require('axios');
-
 const routes = Router()
+const Dev = require('./models/Dev')
 
-routes.get("/devs", async (req, res) => {
-
-    const {github_username} = req.body;
+routes.post("/devs", async (req, res) => {
+    const {github_username, techs, latitude, longitude} = req.body;
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    console.log(apiResponse);
+    const { name = login, avatar_url, bio } = apiResponse.data;
+    const techsArray = techs.split(',').map(tech => tech.trim());
 
-    return res.json({msg: "Hello there"})
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    }
+
+    const dev = await Dev.create({
+        name,
+        github_username,
+        bio,
+        avatar_url,
+        techs: techsArray,
+        location
+    })
+
+    return res.json(dev)
 })
 
 module.exports = routes;
