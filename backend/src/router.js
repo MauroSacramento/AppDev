30c2c125ba16e3004fa3e8bc2d545c0165079d48
Maurosacramento <maurosacramento35@gmail.com>
2025-03-20 17:39:01 +0000

    Finalizar buscar de devs por techs e localização

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,30 +1,11 @@
 const { Router } = require('express');
-const axios = require('axios');
 const routes = Router()
-const Dev = require('./models/Dev')
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
-routes.post("/devs", async (req, res) => {
-    const {github_username, techs, latitude, longitude} = req.body;
+routes.post("/devs", DevController.store)
+routes.get("/devs", DevController.index)
 
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    const { name = login, avatar_url, bio } = apiResponse.data;
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-
-    const dev = await Dev.create({
-        name,
-        github_username,
-        bio,
-        avatar_url,
-        techs: techsArray,
-        location
-    })
-
-    return res.json(dev)
-})
+routes.get("/search", SearchController.index)
 
 module.exports = routes;
